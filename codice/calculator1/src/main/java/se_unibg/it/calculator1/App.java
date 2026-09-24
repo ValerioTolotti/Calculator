fdb602eb1324cce4d6c618c5a0523e0f9f9a70fe
Gabritogni <g.togni9@studenti.unibg.it>
2026-09-24 17:26:58 +0200

    Update App.java
    vale spacca

--- codice/calculator1/src/main/java/se_unibg/it/calculator1/App.java
+++ codice/calculator1/src/main/java/se_unibg/it/calculator1/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        //lavora vale tolotti il capo dei lupi
     }
 }
